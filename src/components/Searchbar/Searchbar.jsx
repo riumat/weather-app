4e90c116bcf1917f9784ecf02fef12ab893efa71
riumat <matteoariu42@gmail.com>
2024-05-15 11:35:59 +0200

    fixed button submit

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -4,20 +4,27 @@ import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 const Searchbar = () => {
-const [city,setCity]=useState("");
-const router=useRouter();
+  const [city, setCity] = useState("");
+  const router = useRouter();
 
-const cityHandler=(cityInput)=>{
-  setCity(cityInput);  
-}
-
-
+  const redirectHandler = (e) => {
+    e.preventDefault();
+    router.push(`/query/${city}`);
+  }
 
   return (
     <div className="flex justify-center ">
+
       <form className="flex justify-between items-center w-full rounded-xl bg-slate-800/70 backdrop-blur">
-        <input type="text" placeholder="Cerca una città..." className="bg-transparent border-none focus:outline-none p-4 xl:p-7 flex-grow" onChange={(e)=>cityHandler(e.target.value)} />
-        <button className="p-5" onClick={()=>router.push(`/query/${city}`)}>
+
+        <input type="text"
+          placeholder="Cerca una città..."
+          className="bg-transparent border-none focus:outline-none p-4 xl:p-7 flex-grow"
+          onChange={(e) => setCity(e.target.value)} />
+
+        <button className="p-5"
+          onClick={(e) => redirectHandler(e)}>
+
           <FaSearch size={20} />
         </button>
       </form>
